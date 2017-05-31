9d137c1fe9b0e6a94c6c3a686e586a43efad1d19
angrifel <angrifel@users.noreply.github.com>
2017-05-31 08:55:45 -0300

    added a unit test for ProviderBoatTowRepository

--- test/Rscue.Api.Tests/Services/ProviderBoatTowRepositoryTests.cs
+++ test/Rscue.Api.Tests/Services/ProviderBoatTowRepositoryTests.cs
@@ -12,15 +12,59 @@
     [Trait("DependsOn", "mongodb")]
     public class ProviderBoatTowRepositoryTests
     {
-        private readonly IProviderBoatTowRepository _boatTowRepository;
+        private readonly IProviderBoatTowRepository _providerBoatTowRepository;
         private ITestDataStore _dataStore;
 
         public ProviderBoatTowRepositoryTests()
         {
             var mongoDatabase = MongoDbHelper.GetRscueCenterUnitTestDatabase();
-            _boatTowRepository = new ProviderBoatTowRepository(mongoDatabase);
+            _providerBoatTowRepository = new ProviderBoatTowRepository(mongoDatabase);
             _dataStore = new MongoTestDataStore(mongoDatabase);
         }
 
+        [Fact]
+        public async Task TestGetByIdAsyncCanRetrieveProviderBoatTow()
+        {
+            // arrange
+            var providerId = Guid.NewGuid().ToString("n");
+            var id = Guid.NewGuid().ToString("n");
+            var provider = new Provider
+            {
+                Id = providerId,
+                City = "Springfield",
+                State = "Illinois",
+                Name = "TowNow!",
+                Email = "call@townow.com",
+                ZipCode = "2342",
+                ProviderImageBucketKey = new ImageBucketKey { Store = "some-store", Bucket = Guid.NewGuid().ToString("n") },
+                Address = "742 Evergreen Terrace",
+            };
+
+            var providerBoatTow = new ProviderBoatTow
+            {
+                Id = id,
+                ProviderId = providerId,
+                FuelCostPerKm = 13,
+                RegistrationNumber = "1234ASD"
+            };
+
+            _dataStore.EnsureProvider(provider);
+            _dataStore.EnsureProviderBoatTow(providerBoatTow);
+
+            // act 
+            var (providerBoatTowResult, outcomeAction, error) = await _providerBoatTowRepository.GetByIdAsync(providerId, id);
+
+            // assert
+            Assert.NotNull(providerBoatTowResult);
+            Assert.Equal(RepositoryOutcomeAction.OkNone, outcomeAction);
+            Assert.Null(error);
+            Assert.Equal(providerBoatTow.Id, providerBoatTowResult.Id);
+            Assert.Equal(providerBoatTow.ProviderId, providerBoatTowResult.ProviderId);
+            Assert.Equal(providerBoatTow.FuelCostPerKm, providerBoatTowResult.FuelCostPerKm);
+            Assert.Equal(providerBoatTow.Name, providerBoatTowResult.Name);
+            Assert.Equal(providerBoatTow.RegistrationNumber, providerBoatTowResult.RegistrationNumber);
+            Assert.Equal(providerBoatTow.BoatModel, providerBoatTowResult.BoatModel);
+            Assert.Equal(providerBoatTow.EngineType, providerBoatTowResult.EngineType);
+        }
     }
 }
